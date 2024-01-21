9209fb1e4d0274028d2ffca5bfcfcfe65a6c97fb
tasuku <taskstudio85@gmail.com>
2024-01-22 01:27:26 +0900

    stage2のtcpファイルをそれぞれ修正。client側の28行目header=sock.recv(32)で止まってしまう

--- tasuku/client_stage1.py
+++ tasuku/client_stage1.py
@@ -1,36 +1,28 @@
 import socket
 import os
+import time
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
 server_address = "127.0.0.1"
 server_port = 9001
 server_address_port = (server_address, server_port)
 address = ""
 port = 9050
-
 sock.bind((address, port))
 
-username = input("Type in your name\n")
-username_len = len(username).to_bytes(1, "big")
+pid2 = os.fork()
+if pid2 > 0:
+    username = input("Type in your name\n")
+    username_len = len(username).to_bytes(1, "big")
 
-try:
     while True:
         message = input("Type in your message\n")
         all_message = username_len + username.encode() + message.encode()
         sock.sendto(all_message, server_address_port)
         print("sending messages")
-        #after making format, send the message
+        time.sleep(1)
+else:
+    while True:
         data, server_address_port = sock.recvfrom(4096)
-        print("receiving", data.decode())
-
-        will_continue = input("Do you continue?\t Y or N\n")
-        if will_continue == 'n' or will_continue == 'N':
-            break
-
-finally:
-    print("closing socket")
-    sock.close()
-
-
-
+        if data:
+            print("receiving : ", data.decode("utf-8"))
--- tasuku/client_stage2_tcp.py
+++ tasuku/client_stage2_tcp.py
@@ -1,8 +1,8 @@
 import socket
 import sys
 
-def protocol_header(room_name, operation, state, payload):
-    return len(room_name).to_bytes(1, "big") + operation.to_bytes(1, "big") + state.to_bytes(1, "big") + len(payload).to_bytes("utf-8")
+def protocol_header(room_name_len, operation, state, payload_len):
+    return room_name_len.to_bytes(1, "big") + int(operation).to_bytes(1, "big") + state.to_bytes(1, "big") + payload_len.to_bytes(29, "big")
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -11,40 +11,44 @@ server_port = 9002
 
 try:
     sock.connect((server_address, server_port))
+    print("conneted with", server_address)
 except socket.error as err:
     print(err)
     sys.exit(1)
 
-try:
-    flag = 0
-    while flag:
-        operation = input("1 : Create a room  2: Join a room")
-        room_name = input("Tyoe in athe room name.")
-        user_name = input("Type in your name")
-        header = protocol_header(room_name, operation, 0, user_name)
-        sock.send(header)
-        sock.send(user_name)
-        # サーバーからのレスポンスを受信
+# try:
+flag = 1
+while flag:
+    operation = input("1 : Create a room  2: Join a room\n")
+    room_name = input("Type in the room name.\n")
+    user_name = input("Type in your name\n")
+    header = protocol_header(len(room_name.encode()), operation, 0, len(user_name.encode()))
+    sock.send(header + user_name.encode("utf-8"))
+    # サーバーからのレスポンスを受信
+    header = sock.recv(32)
+    room_name_len = header[0] #ちなみにheader[:1]という書き方をするとroom_name_len = int.from_bytes(header[:1], "big")と書かないと駄目
+    state = header[2]
+    payload_len = int.from_bytes(header[3:32], "big")
+    sock.recv(room_name_len)
+    server_response = sock.recv(payload_len).decode("utf-8")
+    print(server_response)
+    if state == 0: #stateが0のままのときはリクエストが失敗（とサーバー側で決める。）
+        fleg = 0
+    elif state == 1: #ｓstateが1であればリクエストが成功‥
         header = sock.recv(32)
-        room_name_len = int.from_bytes(header[0], "big")
+        room_name_len = header[0]
+        state = header[2]
         payload_len = int.from_bytes(header[3:32], "big")
         sock.recv(room_name_len)
         server_response = sock.recv(payload_len).decode("utf-8")
         print(server_response)
-        if server_response == "This room does not exist.":
-            fleg = 0
+        flag = 0
         # サーバーから完了のメッセージを受信
-        elif server_address == "The state is 1. Joining" + room_name:
-            header = sock.recv(32)
-            room_name_len = int.from_bytes(header[0], "big")
-            payload_len = int.from_bytes(header[3:32], "big")
-            sock.recv(room_name_len)
-            server_response = sock.recv(payload_len).decode("utf-8")
-            print(server_response)
-            flag = 1
-
-finally:
-    print("closing socket")
-    sock.close()
+        if state == 2:
+            token = server_response.split('\n')[2] #tokenは３行目にあるので、配列の添字は2
+            print(token)
+# finally:
+#     print("closing socket")
+#     sock.close()
 
 # 以降UDP通信が始まる
