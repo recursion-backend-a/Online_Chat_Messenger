c018f1207817ca7122b56d630f877b5348a2154f
kanta-okagawa <flpic0121kp@gmail.com>
2024-02-08 09:00:01 +0900

    added threading

--- main/client_stage2.py
+++ main/client_stage2.py
@@ -7,13 +7,13 @@ import time
 def protocol_header(room_name_len, operation, state, payload_len):
     return room_name_len.to_bytes(1, "big") + int(operation).to_bytes(1, "big") + state.to_bytes(1, "big") + payload_len.to_bytes(29, "big")
 #ソケットを作成
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+tcp_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 #入力されたアドレスのサーバーに接続。ポートは指定しておく
 server_address = input("Type in the server's address to connect to : ")
 server_port = 9002
 
 try:
-    sock.connect((server_address, server_port))
+    tsp_sock.connect((server_address, server_port))
     print("conneted with", server_address)
 except socket.error as err:
     print(err)
@@ -26,24 +26,24 @@ try:
         room_name = input("Type in the room name.\n")
         user_name = input("Type in your name\n")
         header = protocol_header(len(room_name.encode()), operation, 0, len(user_name.encode()))
-        sock.send(header)
-        sock.send(room_name.encode("utf-8"))
-        sock.send(user_name.encode("utf-8"))
+        tcp_sock.send(header)
+        tcp_sock.send(room_name.encode("utf-8"))
+        tcp_sock.send(user_name.encode("utf-8"))
         # サーバーからのレスポンスを受信
-        header = sock.recv(32)
+        header = tcp_sock.recv(32)
         room_name_len = header[0] #ちなみにheader[:1]という書き方をするとroom_name_len = int.from_bytes(header[:1], "big")と書かないと駄目
         state = header[2]
         payload_len = int.from_bytes(header[3:32], "big")
-        server_response = sock.recv(payload_len).decode("utf-8")
+        server_response = tcp_sock.recv(payload_len).decode("utf-8")
         print(server_response)
         if state == 0: #stateが0のままのときはリクエストが失敗（とサーバー側で決める。）
             flag = 1
-        elif state == 1: #ｓstateが1であればリクエストが成功‥
-            header = sock.recv(32)
+        elif state == 1: #stateが1であればリクエストが成功‥
+            header = tcp_sock.recv(32)
             #room_name_len = header[0]
             state = header[2]
             payload_len = int.from_bytes(header[3:32], "big")
-            server_response = sock.recv(payload_len).decode("utf-8")
+            server_response = tcp_sock.recv(payload_len).decode("utf-8")
             print(server_response)
             flag = 0
             # サーバーから完了のメッセージを受信
@@ -51,8 +51,8 @@ try:
                 token = server_response.split('\n')[2] #tokenは３行目にあるので、配列の添字は2
                 print(token)
 finally:
-    print("closing socket")
-    sock.close()
+    print("closing tcp_socket")
+    tcp_sock.close()
 
 # 以降UDP通信が始まる
 
--- main/server_stage2_threading.py
+++ main/server_stage2_threading.py
@@ -1,27 +1,27 @@
 import os 
 import socket
+import threading
 import time
 
 def protocol_header(room_name_len, operation, state, payload_len):
     return room_name_len.to_bytes(1, "big") + int(operation).to_bytes(1, "big") + state.to_bytes(1, "big") + payload_len.to_bytes(29, "big")
 
-pid = os.fork()
+group_hash = {}
 
-if pid > 0:
-    #ソケットを作成し、アドレスとポートを作成
+def tcp_communication():
     tcp_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_address = ''
     server_port = 9002
 
     tcp_sock.bind((server_address, server_port))
-    tcp_sock.listen(1)
+    tcp_sock.listen(2)
 
-    group_hash = {}
     while True:
         print("waiting for connections.")
         connection, client_address = tcp_sock.accept()
         try:
             print("connection from", client_address) 
+
             header = connection.recv(32)
             room_name_len = int.from_bytes(header[:1], "big")  #ちなみにroom_name_len = header[0]という書き方もできる
             operation = int.from_bytes(header[1:2], "big")
@@ -30,7 +30,9 @@ if pid > 0:
             room_name = connection.recv(room_name_len).decode("utf-8")
             user_name = connection.recv(payload_len).decode("utf-8")
             print(room_name_len, operation, state, payload_len, room_name, user_name)
+
             if operation == 1: #操作コードが１のとき(ルーム作成のとき)
+                global group_hash
                 if room_name not in group_hash.keys():
                     group_hash[room_name] = []
                 group_hash[room_name].append((client_address, user_name, "host"))
@@ -42,6 +44,7 @@ if pid > 0:
                 # クライアントにルーム作成の完了を伝える
                 server_message = "The state is 2. Finished making a new room." + '\n' + "Your token is\n" + str(client_address[0])
                 header = protocol_header(len(room_name.encode()), operation, 2, len(server_message.encode()))
+                print(header) #テスト
                 connection.send(header)
                 connection.send(server_message.encode("utf-8"))
 
@@ -53,7 +56,7 @@ if pid > 0:
                     connection.send(header)
                     connection.send(server_message.encode("utf-8"))
                 else:
-                    group_hash[room_name].append((client_address[0], user_name, ""))
+                    group_hash[room_name].append((client_address, user_name, ""))
                     # クライアントに応答
                     server_message = "The state is 1. Joining" + room_name
                     header = protocol_header(len(room_name.encode()), operation, 1, len(server_message.encode()))
@@ -70,21 +73,18 @@ if pid > 0:
             print("Closing current connection")
             connection.close()
 
-else:
-    group_hash = {}
-
+def udp_communication():
     udp_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     server_address = ""
     server_port = 9001
     print("starting up on port {}".format(server_port))
     udp_sock.bind((server_address, server_port))
-    
+
     time_hash = {} #ユーザーの最後のメッセージの時間をハッシュマップで保持
-    
     while True:
-        print("\n waiting to receive message")
+        print("waiting to receive message\n")
         data, client_address = udp_sock.recvfrom(4096)
-        print("room_name_size", data[0]) 
+        print("room_name_size", data[0])
         room_name_len = data[0]
         token_len = data[1]
         room_name = data[2 : 2 + room_name_len].decode("utf-8")
@@ -93,28 +93,33 @@ else:
         print("token", token, type(token))
         print("meesage", message)
         print("client address", client_address)
+        print("room_name", room_name)
         print("group_hash[room_name] : ", group_hash[room_name])
-        if token not in group_hash[room_name]:
-            print("This message is from an unknown user.")
-            continue
-    
-        #以下の処理はルーム作成のときにすべき。グループのハッシュマップにユーザーを登録
-        ##追記：すでにTCPに実装済みのため反映不要
-        # if room_name not in group_hash.keys():
-        #     group_hash[room_name] = []
-        # group_hash[room_name].append(client_address)
-    
-    
+
+
+        # if token not in group_hash[room_name]:
+        #     print("This message is from an unknown user.")
+        #     continue
+
         # now = time.time()
         # time_hash[token] = now
         # # しばらく通信していない相手をuser_address_setから削除
         # for user_token in time_hash.keys():
         #     if time_hash[user_token] - now  > 10000:
         #         group_hash[room_name].remove(user_token)
-    
-    
-        # メッセージをグループ内全員に送信            
+
+        #メッセージをグループ内全員に送信            
         if message:
             for user_token in group_hash[room_name]:
-                sock.sendto(message.encode(), user_token)
-                print("sending the message to {}".format(user_token))
+                udp_sock.sendto(message.encode(), user_token[0])
+                print("sending the message to {}".format(user_token[0]))
+
+
+thread1 = threading.Thread(target=tcp_communication)
+thread2 = threading.Thread(target=udp_communication)
+
+thread1.start()
+thread2.start()
+
+thread1.join()
+thread2.join()
